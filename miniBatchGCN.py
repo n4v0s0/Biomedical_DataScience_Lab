811c5c185f7952adaef5c4eba2e7be8ad31c9a6c
azadi <s6naazad@uni-bonn.de>
2022-12-01 14:23:26 +0100

    Update

--- miniBatchGCN.py
+++ miniBatchGCN.py
@@ -44,30 +44,33 @@ class GCN(torch.nn.Module):
         x = self.convs[-1](x, adj_t)
         return x
 
-def train(model, data, train_idx, optimizer, trainLoader):
+def train(model, device, train_idx, optimizer, trainLoader):
     model.train()
     criterion = torch.nn.BCEWithLogitsLoss()
     totalLoss = 0
     # iterate over data as batches, add up loss and return averaged total loss
     for batch in trainLoader:
+        torch.cuda.empty_cache()
         optimizer.zero_grad()
+        batch = batch.to(device)
         # redefine train_idx to circumvent index out range error for batch
         train_idx = train_idx[:batch.num_nodes]
         out = model(batch.x, batch.adj_t)[train_idx]
-        loss = criterion(out, batch.y[train_idx].to(torch.float))
+        out = out.to(device)
+        loss = criterion(out, batch.y[train_idx].to(torch.half))
         loss.backward()
         totalLoss += loss.item()
         optimizer.step()
+        del batch
 
     return totalLoss/len(trainLoader)
 
 
 @torch.no_grad()
-def test(model, data, split_idx, evaluator):
+def test(model, data, split_idx, evaluator,device):
     model.eval()
-
+    data = data.to(device)
     y_pred = model(data.x, data.adj_t)
-
     train_rocauc = evaluator.eval({
         'y_true': data.y[split_idx['train']],
         'y_pred': y_pred[split_idx['train']],
@@ -80,13 +83,12 @@ def test(model, data, split_idx, evaluator):
         'y_true': data.y[split_idx['test']],
         'y_pred': y_pred[split_idx['test']],
     })['rocauc']
-
+    #del data
+    torch.cuda.empty_cache()
     return train_rocauc, valid_rocauc, test_rocauc
 # %%
-# cant use cpu as device due to memory limit
-# still fighting with cuda aswell :D
-#device = 'cpu'
-#device = torch.device(device)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+device = torch.device(device)
 
 dataset = PygNodePropPredDataset(
     name='ogbn-proteins', transform=T.ToSparseTensor(attr='edge_attr'))
@@ -97,41 +99,40 @@ data.x = data.adj_t.mean(dim=1)
 data.adj_t.set_value_(None)
 
 split_idx = dataset.get_idx_split()
-train_idx = split_idx['train']#.to(device)
+train_idx = split_idx['train'].to(device)
+evaluator = Evaluator(name='ogbn-proteins')
+# Pre-compute GCN normalization.
+adj_t = data.adj_t.set_diag()
+deg = adj_t.sum(dim=1).to(torch.float)
+deg_inv_sqrt = deg.pow(-0.5)
+deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
+adj_t = deg_inv_sqrt.view(-1, 1) * adj_t * deg_inv_sqrt.view(1, -1)
+data.adj_t = adj_t
 
 # %%
 import optuna
+import gc
 from torch_geometric.loader import NeighborLoader
 # this is the routine called by the different optuna trials
 def routine(data,params,trial):
   # init hyperparameters for the trial
+  torch.cuda.empty_cache()
   hidden_channels = params["hidden_channels"]
   num_layers = params["layers"]
   dropout = params["dropout"]
   learning_rate = params["learning_rate"]
   epochs = params["epochs"]
   batch_size = params["batch_size"]
+  num_neighbors = params["neighbors"]
   output_layers = 112
-  model = GCN(data.num_features, hidden_channels, output_layers, num_layers, dropout)
-
-  # Pre-compute GCN normalization.
-  adj_t = data.adj_t.set_diag()
-  deg = adj_t.sum(dim=1).to(torch.float)
-  deg_inv_sqrt = deg.pow(-0.5)
-  deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
-  adj_t = deg_inv_sqrt.view(-1, 1) * adj_t * deg_inv_sqrt.view(1, -1)
-  data.adj_t = adj_t
-  #data = data.to(device)
-  evaluator = Evaluator(name='ogbn-proteins')
+  model = GCN(data.num_features, hidden_channels, output_layers, num_layers, dropout).to(device)
   model.reset_parameters()
-  optimizer = torch.optim.Adam(model.parameters(), learning_rate)
-
-  # datalaoder that takes up to 30 neighbours of each node into account
-  trainLoader = NeighborLoader(data,num_neighbors=[30],batch_size=batch_size,input_nodes=split_idx['train'])
+  optimizer = getattr(torch.optim, params['optimizer'])(model.parameters(), lr= learning_rate)
+  trainLoader = NeighborLoader(data,num_neighbors=[num_neighbors]*params["layers"],batch_size=batch_size,input_nodes=split_idx['train'])
   
   for epoch in tqdm(range(1, epochs+1)):
-    loss = train(model, data, train_idx, optimizer, trainLoader)
-    result = test(model, data, split_idx, evaluator)
+    loss = train(model, device, train_idx, optimizer, trainLoader)
+    result = test(model, data, split_idx, evaluator,device)
     train_rocauc, valid_rocauc, test_rocauc = result
     print(
             f'Epoch: {epoch:02d}, '
@@ -143,27 +144,36 @@ def routine(data,params,trial):
     trial.report(valid_rocauc, epoch)
     if trial.should_prune():
       raise optuna.exceptions.TrialPruned()
-  result = test(model, data, split_idx, evaluator)
+  del trainLoader
+  result = test(model, data, split_idx, evaluator,device)
+  del model
+  gc.collect()
+  torch.cuda.empty_cache()
   train_rocauc, valid_rocauc, test_rocauc = result
   return test_rocauc
 def objective(trial,data):
     params = {
         "learning_rate" : trial.suggest_loguniform("learning_rate", low=10e-5, high=10e-1),
         "layers" : trial.suggest_int("layers", low=3, high=10),
-        # had to choose hidden channels rather low due to my pc crashing on high values :D
-        # should actually be between outputsize and featuresize/10
-        "hidden_channels" : trial.suggest_int("hidden_channels", low=16, high=128),
+        "hidden_channels" : trial.suggest_int("hidden_channels", low=122, high=500),
         "optimizer" : trial.suggest_categorical("optimizier",["Adam","RMSprop","SGD"]),
         "dropout" : trial.suggest_float("dropout", low=0.1, high=0.5, step=0.1),
-        "epochs" : trial.suggest_int("epochs", low=20, high=500),
-        # choose batch size between average degree of nodes and number of nodes
-        # in my case smaller values to prevent pc crashes 
-        "batch_size": trial.suggest_int("batch_size", low=500, high=data.num_nodes/10)
+        "epochs" : trial.suggest_int("epochs", low=20, high=100),
+        "batch_size": trial.suggest_int("batch_size", low=500, high=data.num_nodes/10),
+        "neighbors": trial.suggest_int("neighbors", low=10, high=100)
     }
     print("\nThis trials parameters:\n",params)
     result = routine(data,params,trial)
     return result
 # %%
-study = optuna.create_study(direction="maximize", sampler=optuna.samplers.TPESampler())
-study.optimize(lambda trial: objective(trial,data),n_trials=5)
+# SET PASSWORD FOR SQL
+torch.cuda.empty_cache()
+study = optuna.create_study(
+    direction="maximize",
+    sampler=optuna.samplers.TPESampler(),
+    pruner = optuna.pruners.MedianPruner(n_startup_trials=0),
+    storage="postgresql+psycopg2://postgres:bargeld2@localhost/optunastudy_ogbproteins",
+    load_if_exists=True
+    )
+study.optimize(lambda trial: objective(trial,data),n_trials=30)
 # %%
